7e70bb6544fb88992012652eee014e7df2c0c627
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-12-11 15:15:02 +0400

    backen data

--- src/store/slice/operations.ts
+++ src/store/slice/operations.ts
@@ -3,25 +3,11 @@ import axios from "axios";
 import { Products } from "../../types/productsType";
 axios.defaults.baseURL = "http://localhost:5000";
 
-// export const fetchProducts = createAsyncThunk<Products[]>(
-//   "products/fetchProducts",
-//   async (_, thunkApi) => {
-//     try {
-//       const response = await axios.get("/products");
-//       return response.data;
-//     } catch (error: unknown) {
-//       if (error instanceof Error) {
-//         return thunkApi.rejectWithValue(error.message);
-//       }
-//       return thunkApi.rejectWithValue("An unknown error occurred");
-//     }
-//   }
-// );
 export const fetchProducts = createAsyncThunk<Products[]>(
   "products/fetchProducts",
   async (_, thunkApi) => {
     try {
-      const response = await axios.get("http://localhost:3002/products");
+      const response = await axios.get("/products");
       return response.data;
     } catch (error: unknown) {
       if (error instanceof Error) {
@@ -35,7 +21,7 @@ export const fetchCollectionProducts = createAsyncThunk<Products[]>(
   "products/fetchProducts",
   async (_, thunkApi) => {
     try {
-      const response = await axios.get("http://localhost:3002");
+      const response = await axios.get("/productsCollection");
       return response.data;
     } catch (error: unknown) {
       if (error instanceof Error) {
