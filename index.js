83f1b601ca6cca3bc0f18d5bb860a4bfafbcc91e
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-12-11 15:27:19 +0400

    backen data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const jsonServer = require("json-server"); // importing json-server library
+import jsonServer from "json-server"; // Импортируем json-server
+
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
