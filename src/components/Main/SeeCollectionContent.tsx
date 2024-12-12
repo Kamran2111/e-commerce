a0fb7f46a5182a335d1a69ca3dd315c3327ee767
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-12-12 17:46:59 +0400

    reserve img

--- src/components/Main/SeeCollectionContent.tsx
+++ src/components/Main/SeeCollectionContent.tsx
@@ -1,9 +1,7 @@
 const SeeCollectionContent = () => {
   return (
     <section className="relative">
-      <div className="h-[70vh] sm:h-screen  bg-cover bg-center bg-no-repeat">
-        <img src="/src/assets/images/collection-image.jpg" alt="img" />
-      </div>
+      <div className="bg-custom-img-two h-[70vh] sm:h-screen  bg-cover bg-center bg-no-repeat"></div>
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="bg-black/50 backdrop-blur-sm p-6 rounded-lg text-center max-w-lg mx-4">
           <p className="text-sm sm:text-base font-medium text-white mt-2 leading-relaxed font-montserrat uppercase pb-4">
